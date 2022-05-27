e150968d5de4d544597cc7a6ca115e0516650eec
theAnaKen <the.anaken@gmail.com>
2022-05-27 14:15:47 +0530

    added basic markup

---
+++ src/js/dom.js
@@ -0,0 +1,5 @@
+const parentContainer = document.querySelector(".container");
+const buttons = document.querySelectorAll(".btn");
+const login = document.getElementById("login");
+
+export { parentContainer, buttons, login };
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
-console.log("Hello, World!");
+import { login, buttons, parentContainer } from "./dom";
+
+console.log(parentContainer, buttons, login);
